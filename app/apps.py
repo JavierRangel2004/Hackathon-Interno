0898506bbcdeb73b6c03ce416dbae5d1eea774a7
JavierRangel2004 <0256158@up.edu.mx>
2024-02-05 23:25:30 -0600

    added DB in settings and models

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SustainabilityConfig(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sustainability'
+    name = 'app'
