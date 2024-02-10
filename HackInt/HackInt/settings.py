ef5bc3034d155e8cf553c97b81fdab737d287ac9
JavierRangel2004 <0256158@up.edu.mx>
2024-02-09 23:01:17 -0600

    allowed hosts

--- HackInt/HackInt/settings.py
+++ HackInt/HackInt/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'be53-2806-106e-24-23ba-7d4e-264b-882b-b71c.ngrok-free.app',
     '127.0.0.1',
+    'localhost',
+    'a05e-189-147-66-240.ngrok-free.app',
 ]
 
 
